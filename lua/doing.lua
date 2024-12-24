6cabe3dc8b96faf6af3f997eb944eadee9f54a4f
Hashino <iam.ravara@gmail.com>
2024-12-24 20:47:54 -0300

    refactor: simplified command logic

--- lua/doing.lua
+++ lua/doing.lua
@@ -2,34 +2,24 @@
 local doing = require("doing.core")
 
 local do_cmds = {
-  ---@param args_list string[]
-  ---@param bang boolean
-  ["add"] = function(args_list, bang)
-    -- assembles the rest of the arguments into a string
-    local cmd_args = ""
-    for _, arg in ipairs(args_list) do
-      cmd_args = cmd_args .. arg .. " "
-    end
-
-    doing.add(cmd_args, bang)
-  end,
+  ["add"] = doing.add,
+  ["edit"] = doing.edit,
+  ["done"] = doing.done,
+  ["toggle"] = doing.toggle,
 
   ["status"] = function()
     vim.notify(doing.status(true), vim.log.levels.INFO, { title = "doing.nvim", })
   end,
-
-  ["edit"] = doing.edit,
-  ["done"] = doing.done,
-  ["toggle"] = doing.toggle,
 }
 
 -- sets up the `:Do` command
 vim.api.nvim_create_user_command("Do", function(args)
-  local args_list = vim.split(args.args, "%s+", { trimempty = true, })
-  local cmd = table.remove(args_list, 1)
+  -- split the command and the arguments by the first space
+  local cmd = args.args:sub(1, args.args:find(" ") or (#args.args + 1) - 1)
+  local cmd_args = args.args:sub(#cmd + 2) or ""
 
   if vim.tbl_contains(vim.tbl_keys(do_cmds), cmd) then
-    do_cmds[cmd](args_list, args.bang)
+    do_cmds[cmd](cmd_args, args.bang)
   else
     vim.notify("invalid command: " .. cmd, vim.log.levels.ERROR,
       { title = "doing.nvim", })

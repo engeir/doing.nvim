f57357e755c05a055eb8e59d34f1db77ebc04af4
Hashino <iam.ravara@gmail.com>
2025-01-12 16:20:13 -0300

    docs(default_opts): fixed wrong documentation
    still had documentation for a test config that was never released

--- lua/doing/config.lua
+++ lua/doing/config.lua
@@ -1,14 +1,13 @@
 local Config = {}
 
----@class DoingOptions
----@field ignored_buffers string[]|fun():string[] elements are checked against buffer filetype/filename/filepath
----@field message_timeout integer how many millisecons messages will stay on status
+---@class DoingOptions config for [Hashino/doing.nvim]
 ---@field doing_prefix string prefix to show before the task
+---@field ignored_buffers string[]|fun():string[] elements are checked against buffer filetype/filename/filepath
+---@field show_remaining boolean show "+n more" when there are more than 1 tasks
+---@field show_messages boolean show messages in status string
+---@field message_timeout integer how many millisecons messages will stay on status
 ---@field winbar.enabled boolean if plugin should manage the winbar
 ---@field store.file_name string name of the task file
----@field store.auto_delete_file boolean auto delete tasks file
----@field show_messages boolean show messages in status string
----@field show_remaining boolean show "+n more" when there are more than 1 tasks
 ---@field edit_win_config vim.api.keyset.win_config window configs of the floating editor
 
 ---@class DoingOptions

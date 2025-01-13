0845b214601a90ef7ed88373c6fe5ec5fb4813bc
Hashino <iam.ravara@gmail.com>
2025-01-12 21:19:22 -0300

    feat(config): always sync tasks file to tasks
    when true, syncs tasks file on every tasks change
    when false, only syncs tasks file on `VimLeave`
    defaults to false

--- lua/doing/state.lua
+++ lua/doing/state.lua
@@ -1,64 +1,53 @@
 local config = require("doing.config")
 local utils = require("doing.utils")
-local separator = utils.get_path_separator()
+
+local tasks_file = ""
+
+local function import_tasks()
+  tasks_file = vim.fn.getcwd()
+     .. utils.os_path_separator()
+     .. config.options.store.file_name
+
+  local ok, res = pcall(vim.fn.readfile, tasks_file)
+  return ok and res or {}
+end
 
 local State = {}
 
-State.file = nil
 State.message = nil
 State.view_enabled = true
-State.file_name = config.options.store.file_name
 
----finds tasks file in cwd
-local function import_file()
-  local file = vim.fn.findfile(vim.fn.getcwd() .. separator .. State.file_name, ".;")
+State.tasks = import_tasks()
 
-  if file == "" then
-    State.file = nil
-  else
-    assert(vim.fn.filereadable(file), file .. ": file not found or not readable")
-    State.file = file
-  end
-
-  return State.file and vim.fn.readfile(State.file) or {}
-end
-
-State.tasks = import_file()
-
+-- reloads tasks when directory changes
 vim.api.nvim_create_autocmd("DirChanged", {
   callback = function()
-    State.tasks = import_file()
+    State.tasks = import_tasks()
     utils.task_modified()
   end,
 })
 
 ---syncs file tasks with loaded tasks
 local function sync()
-  if (not State.file) and #State.tasks > 0 then
-    -- if file doesn't exist and there are tasks, create it
-    local name = State.file_name
-    local cwd = vim.fn.getcwd()
-    local file = io.open(cwd .. separator .. name, "w")
-    assert(file, "couldn't create " .. name .. " in current cwd: " .. cwd)
-
-    file:write("")
-    file:close()
-
-    State.file = cwd .. separator .. name
-  elseif State.file and #State.tasks == 0 then
+  if vim.fn.findfile(tasks_file, ".;") ~= "" and #State.tasks == 0 then
     -- if file exists and there are no tasks, delete it
     vim.schedule_wrap(function()
-      local success, err, err_name = (vim.uv or vim.loop).fs_unlink(State.file)
+      local success, err, err_name = (vim.uv or vim.loop).fs_unlink(tasks_file)
 
       if not success then
-        utils.notify(tostring(err_name) .. ":" .. tostring(err), vim.log.levels.ERROR)
+        utils.notify(tostring(err_name) .. ":" .. tostring(err),
+          vim.log.levels.ERROR)
       end
     end)()
   end
 
-  if State.file and vim.fn.writefile(State.tasks, State.file) ~= 0 then
+  if #State.tasks > 0 and not vim.fn.writefile(State.tasks, tasks_file) then
     utils.notify("error writing to tasks file", vim.log.levels.ERROR)
   end
+end
+
+if not config.options.store.sync_tasks then
+  vim.api.nvim_create_autocmd("VimLeave", { callback = sync, })
 end
 
 function State.add(str, to_front)
@@ -67,18 +56,18 @@ function State.add(str, to_front)
   else
     table.insert(State.tasks, str)
   end
-
-  sync()
+  return config.options.store.sync_tasks and sync()
 end
 
 function State.done()
   table.remove(State.tasks, 1)
-  sync()
+  return config.options.store.sync_tasks and sync()
 end
 
 function State.set(tasks)
   State.tasks = tasks
-  sync()
+  vim.notify(tasks_file)
+  return config.options.store.sync_tasks and sync()
 end
 
 return State
